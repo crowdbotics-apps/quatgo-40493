3cd9635cb0ee6f64fc77cfcd490e6582dee9cdf1
Crowdbotics <team@crowdbotics.com>
2023-05-06 09:24:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quatgo-40493",
+  url: "https://quatgo-40493.botics.co",
+  api: "https://quatgo-40493.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
